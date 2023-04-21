f67a45bf83b5aaaec932cfaaea7917efde5d5cbb
Saffian-Asghar <baigsaffian@gmail.com>
2023-04-21 10:36:17 +0200

    Adding derivative

--- project/app.py
+++ project/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import numpy as np
+import plotly.graph_objects as go
 
 @st.cache_data
 def load_data(url):
@@ -40,11 +41,25 @@ fig = px.line(df_grouped, x='date', y='new_cases_per_million', color='location',
               range_x=[start_date, end_date])
 
 if variable == 'Cumulative Count':
-
+    selected_country = selected_countries[0]
     if selected_option == 'Cases':
         fig.update_traces(y=df_grouped['total_cases_per_million'])
+        daily_change = 20 * df_grouped.groupby('location')['total_cases_per_million'].diff().fillna(0)
     else:
         fig.update_traces(y=df_grouped['total_deaths_per_million'])
+        daily_change = 20 * df_grouped.groupby('location')['total_deaths_per_million'].diff().fillna(0)
+        
+    daily_change_fig = px.line(df_grouped, x='date', y=daily_change, color='location',
+                               labels={'y': f"Daily change in {selected_option.lower()} {variable.lower()} per million"},
+                               title=f"Daily change in {selected_option.capitalize()} {variable.capitalize()} of COVID-19 per million",
+                               range_x=[start_date, end_date])
+    fig.add_trace(go.Scatter(x=df_grouped.loc[df_grouped['location']==selected_country, 'date'], 
+                                      y=daily_change, 
+                                      mode='lines', 
+                                      line=dict(color='grey', width=2),
+                                      name=f'{selected_country} - Derivative'))
+    # fig.add_trace(daily_change_fig['data'][0], line=go.scatter.Line(color="gray"))
+
 elif variable == '7-Day Rolling Average':
     window_size = 7
     if selected_option == 'Cases':
